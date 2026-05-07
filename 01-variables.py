ee74a225b951ee81ef17946926f37b4359fac5da
germanseiyae <germancristobal.cortes@alumnos.ulagos.cl>
2026-05-07 14:56:19 -0400

    update

--- 01-variables.py
+++ 01-variables.py
@@ -3,4 +3,3 @@ apellido = "Cortés"
 edad = "18"
 
 print("Mi nombre y apellido es",nombre, apellido,"y mi edad es", edad)
-
---
+++ 02-tiposdedatos.py
@@ -0,0 +1,4 @@
+#Numero entero
+edad = 18
+#Numero Flotante (Real)
+estatura = 1.75
