7665c4e9102f288e7f61a83d96509f378406f484
germanseiyae <germancristobal.cortes@alumnos.ulagos.cl>
2026-05-11 12:26:37 -0400

    update

--- 02-tiposdedatos.py
+++ 02-tiposdedatos.py
@@ -2,3 +2,20 @@
 edad = 18
 #Numero Flotante (Real)
 estatura = 1.74
+
+#numeros complejos
+num_complejo = 4 + 2j        #primera forma de crear un numero complejo
+otro_complejo = complex(4.2) #segunda forma de crear un numero complejo
+
+print(num_complejo)
+print(otro_complejo)
+
+base = 8
+altura = 12.5
+
+area = (base * altura) / 2
+print(f"El area del triangulo es de {area} cm")
+
+#Salida de numeros en PI
+PI = 3.141592653589793
+print(f"El numero PI tiene un valor de {PI: .2f}")
