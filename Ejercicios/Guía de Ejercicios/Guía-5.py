db13ef4764f7de42bacc223a23ec5771d87c8895
germanseiyae <germancristobal.cortes@alumnos.ulagos.cl>
2026-06-02 12:03:10 -0400

    update

---
+++ Ejercicios/Guía de Ejercicios/Guía-5.py
@@ -0,0 +1,15 @@
+"""5. Una plataforma web de la Universidad de Los Lagos mide la velocidad de respuesta
+de su servidor de asignación de asignaturas. Se han tomado 3 muestras de tiempo de
+respuesta (en milisegundos) de forma manual. Escribe un programa en Python que:
+a) Solicite al administrador de la plataforma ingresar por terminal los 3 tiempos de
+respuesta (los cuales pueden contener decimales, tipo float).
+b) Almacene los 3 valores ingresados dentro de una lista de Python que debe tener
+por nombre tiempos respuesta.
+c) Acceda por medio de sus índices ([0], [1], [2]) a los elementos de la lista para
+calcular el tiempo promedio de respuesta del servidor.
+d ) Encuentre el tiempo más rápido (mínimo) y el tiempo más lento (máximo) utili-
+zando las funciones propias de Python.
+e) Calcule la “brecha de rendimiento”, que corresponde a la resta entre el tiempo
+máximo y el mínimo.
+f ) Imprima en pantalla la lista completa de datos y el reporte con el promedio y la
+brecha calculada."""
