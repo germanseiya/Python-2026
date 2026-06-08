285c5b09fd9cdfd3c60b90e10d1cb99d20659c4e
germanseiyae <germancristobal.cortes@alumnos.ulagos.cl>
2026-06-08 12:39:31 -0400

    update

--- 05-Conjuntos.py
+++ 05-Conjuntos.py
@@ -2,5 +2,8 @@ colores_primarios = {"Azul", "Rojo", "Amarillo"}
 colores_secundarios = set(["Verde", "Naranja", "Violeta"])
 print(type(colores_primarios))
 
-print(colores_primarios)
-print(colores_secundarios)
+print("Conjunto 1: (colores_primarios)")
+print("Conjunto 2: (colores_secundarios)")
+
+colores_nuevos = {"Café", "Gris", "Azul", "Rojo", "Azul"}
+print("Conjunto 3: (colores_nuevos)")
